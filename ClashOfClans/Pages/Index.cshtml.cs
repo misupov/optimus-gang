45106418a107fab1324cfd04f42eec2a8e8d6f98
Mikhail Isupov <lam0x86@gmail.com>
2018-09-29 03:16:33 +0300

    +1

--- ClashOfClans/Pages/Index.cshtml.cs
+++ ClashOfClans/Pages/Index.cshtml.cs
@@ -14,12 +14,12 @@ namespace ClashOfClans.Pages
         {
             using (var httpClient = new HttpClient(new SocketsHttpHandler()))
             {
-                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",
-                    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjhiMjQwMTI5LTYzNTMtNDRkOC05ZDNmLWZkN2E2ZWU5ZDlkOSIsImlhdCI6MTUzODE2MjY4Mywic3ViIjoiZGV2ZWxvcGVyLzc3OTFkNTU4LTRhOTgtY2I3NS1kNjJkLWIxNTdiMThiN2E0MyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE5NS4xMzMuNzMuMjMiLCI0Ni4zOS4yNDguNjAiXSwidHlwZSI6ImNsaWVudCJ9XX0.kdDD02LNuzY2VY8pKzFmIoVzYQXWlxNBtr_5oiGjXO3NF7NWwitypb7YdvXZGxGPcVSIpIRofLZ0s3Psb8fzhA");
-                var response = await httpClient.GetAsync("https://api.clashofclans.com/v1/clans/%23Q8GRU2LR");
-                response.EnsureSuccessStatusCode();
-                var responseBody = await response.Content.ReadAsAsync<ClanDetails>();
-                ClanDetails = responseBody;
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjhiMjQwMTI5LTYzNTMtNDRkOC05ZDNmLWZkN2E2ZWU5ZDlkOSIsImlhdCI6MTUzODE2MjY4Mywic3ViIjoiZGV2ZWxvcGVyLzc3OTFkNTU4LTRhOTgtY2I3NS1kNjJkLWIxNTdiMThiN2E0MyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE5NS4xMzMuNzMuMjMiLCI0Ni4zOS4yNDguNjAiXSwidHlwZSI6ImNsaWVudCJ9XX0.kdDD02LNuzY2VY8pKzFmIoVzYQXWlxNBtr_5oiGjXO3NF7NWwitypb7YdvXZGxGPcVSIpIRofLZ0s3Psb8fzhA");
+                var clan = await httpClient.GetAsync("https://api.clashofclans.com/v1/clans/%23Q8GRU2LR");
+                clan.EnsureSuccessStatusCode();
+                var clanDetails = await clan.Content.ReadAsAsync<ClanDetails>();
+                ClanDetails = clanDetails;
+                var warlog = await httpClient.GetAsync("https://api.clashofclans.com/v1/clans/%23Q8GRU2LR/warlog");
             }
         }
 
