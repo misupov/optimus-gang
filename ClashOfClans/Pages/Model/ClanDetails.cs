a254db74fea7808e21a8c6562014dcec713512ca
Mikhail Isupov <lam0x86@gmail.com>
2018-09-29 05:45:21 +0300

    +1

--- ClashOfClans/Pages/Model/ClanDetails.cs
+++ ClashOfClans/Pages/Model/ClanDetails.cs
@@ -5,7 +5,7 @@ namespace ClashOfClans.Pages.Model
     public class ClanDetails
     {
         public string Tag { get; set; }
-        public string Name;
+        public string Name { get; set; }
         public Location Location { get; set; }
         public IconUrls BadgeUrls { get; set; }
         public int ClanLevel { get; set; }
@@ -22,5 +22,9 @@ namespace ClashOfClans.Pages.Model
         public bool IsWarLogPublic { get; set; }
         public string Description { get; set; }
         public IReadOnlyList<Member> MemberList { get; set; }
+        public int Attacks { get; set; }
+        public int Stars { get; set; }
+        public int ExpEarned { get; set; }
+        public  double DestructionPercentage { get; set; }
     }
 }
---
+++ ClashOfClans/Pages/Model/WarlogDetails.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace ClashOfClans.Pages.Model
+{
+    [UsedImplicitly]
+    public class WarlogDetails
+    {
+        public IReadOnlyList<WarlogDetail> Items { get; set; }
+    }
+
+    [UsedImplicitly]
+    public class WarlogDetail
+    {
+        public WarResult Result { get; set; }
+        public string EndTime { get; set; }
+        public int TeamSize { get; set; }
+        public ClanDetails Clan { get; set; }
+        public ClanDetails Opponent { get; set; }
+    }
+
+    public enum WarResult
+    {
+        Win,
+        Lose,
+        Tie,
+    }
+}
