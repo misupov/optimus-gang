f6bb275d2a0b9b8135a876c0e4328b70b5615464
Mikhail Isupov <lam0x86@gmail.com>
2018-09-29 07:58:19 +0300

    no message

--- ClashOfClans/wwwroot/js/site.js
+++ ClashOfClans/wwwroot/js/site.js
@@ -1,4 +1,5 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your Javascript code.
+﻿navigator.serviceWorker.register("/js/sw.js")
+    .then(() => navigator.serviceWorker.ready.then((worker) => {
+        worker.sync.register("syncdata");
+    }))
+    .catch((err) => console.log(err));
