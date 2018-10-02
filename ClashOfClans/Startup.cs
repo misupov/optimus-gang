89be6b79588eddef454b0bf1aa427d9f1320232c
Mikhail Isupov <lam0x86@gmail.com>
2018-10-03 02:49:12 +0300

    no message

--- ClashOfClans/Startup.cs
+++ ClashOfClans/Startup.cs
@@ -38,15 +38,7 @@ namespace ClashOfClans
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
-            }
+            app.UseDeveloperExceptionPage();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
