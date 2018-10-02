7796320c6638eaa5eecdc3d2334f5a2f2506710d
Mikhail Isupov <lam0x86@gmail.com>
2018-10-03 01:15:52 +0300

    +1

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,23 @@
+function Stop-AppPool ($webAppPoolName, [int]$secs) {
+    $retvalue = $false
+    $wsec = (get-date).AddSeconds($secs)
+    Stop-WebAppPool -Name $webAppPoolName
+    Write-Output "$(Get-Date) waiting up to $secs seconds for the WebAppPool '$webAppPoolName' to stop"
+    $poolNotStopped = $true
+    while (((get-date) -lt $wsec) -and $poolNotStopped) {
+        $pstate = Get-WebAppPoolState -Name $webAppPoolName
+        if ($pstate.Value -eq "Stopped") {
+            Write-Output "$(Get-Date): WebAppPool '$webAppPoolName' is stopped"
+            $poolNotStopped = $false
+            $retvalue = $true
+        }
+    }
+    return $retvalue
+}
+
 git pull
 dotnet publish
-Stop-WebSite -Name "clash.lam0x86.ru"
+Stop-AppPool -Name "clash.lam0x86.ru"
+Start-Sleep 2
 Copy-Item "ClashOfClans\bin\Debug\netcoreapp2.1\publish\*" -Destination "C:\WebSites\clash.lam0x86.ru\" -Recurse -Force
-Start-WebSite -Name "clash.lam0x86.ru"
+Start-WebAppPool -Name "clash.lam0x86.ru"
