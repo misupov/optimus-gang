cc17b662db8d8e3d50cdfde23d56cf8026ace126
Mikhail Isupov <lam0x86@gmail.com>
2018-10-03 01:44:32 +0300

    no message

--- ClashOfClans/wwwroot/site.js
+++ ClashOfClans/wwwroot/site.js
@@ -1,3 +1,11 @@
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("/sw.js");
 }
+
+window.addEventListener("beforeinstallprompt", function (e) {
+    // log the platforms provided as options in an install prompt 
+    console.log(e.platforms); // e.g., ["web", "android", "windows"] 
+    e.userChoice.then(function (outcome) {
+        console.log(outcome); // either "accepted" or "dismissed"
+    });
+});
